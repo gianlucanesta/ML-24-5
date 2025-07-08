4c371c51137208168c3c9107620962142f69f1ce
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2025-07-08 22:45:28 +0200

    Add code coverage reports and update App component
    - Introduced code coverage reports with generated HTML, CSS, and JS files.
    - Added favicon and sort arrow sprite images for the coverage report.
    - Removed unused jest.config.js file.
    - Updated App component to simplify structure and remove unnecessary imports.
    - Added setupTests.js for jest-dom matchers.
    - Updated App.test.js to include a new test case for the App component.

---
+++ my-react-app/src/setupTests.js
@@ -0,0 +1,5 @@
+// jest-dom aggiunge matcher personalizzati per jest per asserire sui nodi del DOM.
+// ti permette di fare cose come:
+// expect(element).toHaveTextContent(/react/i)
+// scopri di più: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom";
