62a63987e0203a2c15df2068bf9a5b0e4c15cfc8
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2025-07-08 22:27:48 +0200

    feat: add file mock for general image types in jest configuration

--- my-react-app/jest.config.js
+++ my-react-app/jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-    "^/vite\\.svg$": "<rootDir>/__mocks__/viteMock.js",
+    "^/vite\\.svg$": "<rootDir>/__mocks__/viteMock.js", // Mock vite.svg
+    "^.+\\.(svg|png|jpg|jpeg|gif|webp|avif|ico|bmp|tiff)$":
+      "<rootDir>/__mocks__/fileMock.js", // General image mock
   },
   testEnvironment: "jsdom",
 };
